fd80a5849157ef19ee5ee07dde1ef438a37ad6a3
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-05-11 22:32:02 +0300

    Add HC logo

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <img id="hc-logo" />
     </div>
     <div className="content">
       <div className="inner">
