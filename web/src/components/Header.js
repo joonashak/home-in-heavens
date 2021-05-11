345f380eb40b801de0de6f19a0fec395f7ee3027
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-05-11 18:55:17 +0300

    Add some budding text content to frontpage

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -8,13 +8,12 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Home In Heavens</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          Wormhole hosting service by Hole Control
+        </p>
+        <p>
+          Make ISK and get fights in W-space without all the stress and hassle of owning and defending your own wormhole system!
         </p>
       </div>
     </div>
